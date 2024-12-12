f9029cf4cd7b6abf293c06da5e72d6be59e247dd
Y N V Siva Ramkumar Reddy <ynvsivaramkumar.r@ad.infosys.com>
2024-12-12 18:05:38 +0530

    added lines

--- home.php
+++ home.php
@@ -1,7 +1,9 @@
 <?php
-// We need to use sessions, so you should always start sessions using the below code.
+// We need to use sessions, so you should always start sessions using the below code.git 
 session_start();
 // If the user is not logged in redirect to the login page...
+// If the user is not logged in redirect to the login page...
+// If the user is not logged in redirect to the login page...git 
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit;
